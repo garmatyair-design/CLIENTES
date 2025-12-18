5cbab39de5a0b7d20e4d9182d02fa0b3e6abd844
garmatyair-design <garmat.yair@gmail.com>
2025-12-17 18:06:46 -0600

    Update js-app.js

--- js-app.js
+++ js-app.js
@@ -1,5 +1,6 @@
-const KEY_CLIENTS = 'crm_clients';
-const KEY_PROJECTS = 'crm_projects';
+const KEY_CLIENTS = 'crm_clients_v3';
+const KEY_PROJECTS = 'crm_projects_v3';
+
 
 
 let clients = JSON.parse(localStorage.getItem(KEY_CLIENTS) || '[]');
